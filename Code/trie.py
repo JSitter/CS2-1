23dfd8e785f6cc7f6bd105908fdae529a2ea8b1b
Justin Sitter <justin@jaytria.com>
2019-02-13 08:34:16 -0800

    refactored array for dictionary

--- Code/trie.py
+++ Code/trie.py
@@ -4,22 +4,12 @@ class Trie:
   def __init__(self):
     self.root = Trie_Node()
   
-  def char_to_index(self, character):
-    char = character.lower()
-    # ASCII A = 97 so subtract 97
-    index = (ord(char) - 97)
-    if index < 0 or index > 25:
-      raise Exception('Character out of range: Words can only contain characters a-z.')
-    return index
-  
   def insert(self, word):
     cur_node = self.root
     for index, char in enumerate(word):
-      # returns True if character already appears
-      char_index = self.char_to_index(char)
-      if cur_node.children[char_index]:
+      if char in cur_node.children:
         # Descend into child node
-        cur_node = cur_node.children[char_index]
+        cur_node = cur_node.children[char]
         if len(word) - 1 == index:
           cur_node.word_end = True
       else:
@@ -27,35 +17,33 @@ class Trie:
         new_node = Trie_Node()
         if len(word) - 1 == index:
           new_node.word_end = True
-        cur_node.children[char_index] = new_node
-        cur_node = cur_node.children[char_index]
+        cur_node.children[char] = new_node
+        cur_node = cur_node.children[char]
   
   def search(self, word):
     cur_node = self.root
     for index, char in enumerate(word):
-      char_index = self.char_to_index(char)
-      # Last character of word should have a flag set
-      if len(word) - 1 == index:
-        if cur_node.children[char_index].word_end:
-          return True
+      if char in cur_node.children:
+        # Last character of word should have a flag set
+        if len(word) - 1 == index:
+          if cur_node.children[char].word_end:
+            return True
+          else:
+            return False
         else:
-          return False
+          # Not at last character yet
+          cur_node = cur_node.children[char]
       else:
-        # Not at last character yet
-        if cur_node.children[char_index]:
-          cur_node = cur_node.children[char_index]
-        else:
-          # Character index doesn't exist return false immediately 
-          return False
+        # Character index doesn't exist return false immediately 
+        return False
 
 
   def autocomplete(self, prefix):
     cur_node = self.root
     words = []
     for char in prefix:
-      char_index = self.char_to_index(char)
-      if cur_node.children[char_index]:
-        cur_node = cur_node.children[char_index]
+      if char in cur_node.children:
+        cur_node = cur_node.children[char]
       else:
         # Haven't gotten to end of prefix therefore no similar words exist
         return words
@@ -71,4 +59,4 @@ class Trie_Node:
     # End of word defaults to False
     self.word_end = False
     # Initialize starting values to None
-    self.children = [None] * 26
+    self.children = {}
--- Code/trie_test.py
+++ Code/trie_test.py
@@ -1,22 +1,24 @@
 #!python
 from trie import Trie
 
-def test_char_to_index():
-  t = Trie()
-  assert t.char_to_index('a') == 0
-  assert t.char_to_index('A') == 0
-  assert t.char_to_index('Z') == 25
+# def test_char_to_index():
+#   t = Trie()
+#   assert t.char_to_index('a') == 0
+#   assert t.char_to_index('A') == 0
+#   assert t.char_to_index('Z') == 25
   
 
 def test_insert():
   t = Trie()
   t.insert("Apogee")
-  assert t.root.children[0]
+  assert t.root.children['A']
+  assert t.root.children['A'].children['p'].children['o'].children['g'].children['e'].children['e']
+  assert t.root.children['A'].children['p'].children['o'].children['g'].children['e'].children['e'].word_end
 
 def test_search():
   t = Trie()
   t.insert("Astral")
-  assert t.search("astral")
+  assert t.search("Astral")
   assert not t.search("Papaya")
 
 def test_find_similar():
