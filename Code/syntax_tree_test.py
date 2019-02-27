5d2e73174aeeec26dba98cf312a6b763611a6437
Justin Sitter <justin@jaytria.com>
2019-02-27 01:26:51 -0800

    update test to reflect changes

---
+++ Code/syntax_tree_test.py
@@ -0,0 +1,15 @@
+from syntax_tree import ASTree
+
+def test_init():
+  t = ASTree()
+  assert t.root == None
+ 
+
+def test_insert():
+  t = ASTree()
+
+  t.parse("3+4*5")
+  t.parse("23+56*2")
+
+  t.parse("2+ 4")
+  assert t.evaluate() == 6
