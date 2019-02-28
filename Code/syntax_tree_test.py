af4fdc9c4cf21eb1a930fc12fe120634358c76cd
Justin Sitter <justin@jaytria.com>
2019-02-28 00:40:50 -0800

    add tests for parsing and evaluating multiple calulations

--- Code/syntax_tree_test.py
+++ Code/syntax_tree_test.py
@@ -4,20 +4,45 @@ def test_init():
   t = ASTree()
   assert t.root == None
  
+def test_parse_single_addition():
+  t = ASTree()
+  t.parse("2+3")
+  assert t.root.symbol == "+"
+  assert t.root.left.value == "2"
+  assert t.root.right.value == "3"
 
-def test_insert():
+def test_evaluate_single_addition():
+  t = ASTree("4+5")
+  assert t.evaluate() == float(9)
+
+def test_parse_multi_addition():
+  t = ASTree()
+  t.parse("2+3+5")
+  assert t.root.symbol == "+"
+  assert t.root.left.symbol == "+"
+  assert t.root.right.value == "5"
+  assert t.root.left.left.value == "2"
+  assert t.root.left.right.value == "3"
+
+def test_parse_multi_varied():
   t = ASTree()
 
   t.parse("348+47*5501")
-  assert t.root.symbol == '+'
-  assert t.root.left.value == "348"
-  assert t.root.right.value == None
-  assert t.root.right.symbol == "*"
+  assert t.root.symbol == '*'
+  assert t.root.right.value == "5501"
+  assert t.root.left.symbol == "+"
+  assert t.root.left.value == None
+  
 
   t.parse("23+56*2")
 
   t.parse("2+ 4")
   assert t.root.symbol == "+"
+
+def test_evaluate_multi_varied():
+  t = ASTree()
+  t.parse("47*5501+348")
+  assert t.evaluate() == float(258895)
 
 def test_evaluate():
   t = ASTree()
