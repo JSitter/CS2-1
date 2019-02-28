3781aea8db45738491a4453c0f75374067f7ce87
Justin Sitter <justin@jaytria.com>
2019-02-27 16:56:46 -0800

    add valid tests

--- Code/syntax_tree_test.py
+++ Code/syntax_tree_test.py
@@ -8,8 +8,20 @@ def test_init():
 def test_insert():
   t = ASTree()
 
-  t.parse("3+4*5")
+  t.parse("348+47*5501")
+  assert t.root.symbol == '+'
+  assert t.root.left.value == "348"
+  assert t.root.right.value == None
+  assert t.root.right.symbol == "*"
+
   t.parse("23+56*2")
 
   t.parse("2+ 4")
   assert t.root.symbol == "+"
+
+def test_evaluate():
+  t = ASTree()
+  t.create_test_tree()
+  evaluated_answer = t.evaluate()
+  print("Answer: {}".format(evaluated_answer))
+  assert evaluated_answer == float(31)
