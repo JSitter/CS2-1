7f01d5eff5574a3c2028a161c15d57a1ea9d6711
Justin Sitter <justin@jaytria.com>
2019-03-03 18:48:42 -0800

    add method stubs

--- Code/avl_tree.py
+++ Code/avl_tree.py
@@ -1,6 +1,15 @@
 class AVL_Tree:
   def __init__(self):
     self.root = None
+  
+  def insert(self, node):
+    pass
+  
+  def left_rotation(self, node):
+    pass
+  
+  def right_rotation(self, node):
+    pass
 
 class AVL_Node:
   def __init__(self):
