4dd41c9f55e2bf16376ded248d8bb5ce92e05127
Justin Sitter <justin@jaytria.com>
2019-03-10 19:13:54 -0700

    add tests for priority queue

---
+++ Code/priority_queue_test.py
@@ -0,0 +1,31 @@
+from priorityqueue import PriorityQueue, PriorityNode
+
+def test_instance():
+  pq = PriorityQueue()
+  assert pq.heap
+
+def test_single_enqueue():
+  pq = PriorityQueue()
+  pq.enqueue(45, 3)
+  assert pq.heap.items[0].value == 45
+  assert pq.heap.items[0].priority == 3
+
+def test_dequeue():
+  pq = PriorityQueue()
+  pq.enqueue(45, 3)
+  assert pq.dequeue().value == 45
+
+def test_multi_enqueue():
+  pq = PriorityQueue()
+  pq.enqueue(45, 3)
+  pq.enqueue(50, 2)
+  pq.enqueue(2, 5)
+  pq.enqueue(2, 1)
+
+  assert pq.dequeue().value == 2
+  print(pq)
+  assert pq.dequeue().value == 50
+  assert pq.dequeue().value == 45
+  assert pq.dequeue().value == 2
+
+
